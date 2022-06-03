66a0c7d5212db5aa53e9637d83476e743c23bf48
Sapozhkov_Evgenii <sapozhkovevgenii@gmail.com>
2022-06-03 12:34:28 +0300

    using templates

--- fsite.py
+++ fsite.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+menu = [{"name": "Setup", "url": "install-flask"},
+        {"name": "First app", "url": "first-app"},
+        {"name": "Feedback", "url": "contact"}]
+
 
 @app.route("/")
 def index():
-    return "index"
+    return render_template('index.html', title='About Flask', menu=menu)
 
 
 @app.route("/about")
 def about():
-    return "<h1>Про Flask</h1>"
+    return render_template('about.html', title='About site', menu=menu)
+
+
+@app.route("/contact", methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        print(request.form['email'])
+    return render_template('contact.html', title='Feedback', menu=menu)
 
 
 if __name__ == "__main__":
